21632d52024d8fe41feb925fdde3a2e679be3d37
php-fast <phpswoole@gmail.com>
2024-10-02 03:48:49 +0700

    Update AuthController.php

--- application/Controllers/Backend/AuthController.php
+++ application/Controllers/Backend/AuthController.php
@@ -17,7 +17,6 @@ class AuthController extends BaseController
     protected $usersModel;
     protected $assets;
     protected $mailer;
-    protected $t;
 
     public function __construct()
     {
