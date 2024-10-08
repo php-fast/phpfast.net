0f15c346621cb19773407301292aefb6c166e0d1
Nguyen-1210 <ghetcongai137@gmail.com>
2024-10-08 14:19:09 +0700

    [RAP-LAYOUT-AUTH]

---
+++ application/Languages/vi/Auth.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    //common
+    'csrf_failed'   =>  'Request CSRF timeout or not valid by double URL request!',
+    'username'  =>  'Tài khoản',
+    'password'  =>  'Mật khẩu',
+
+    //auth
+    'login'    =>  'Đăng nhập',
+    'register' =>  'Đăng ký',
+    'forgot_pass'   =>  'Forgot Password',
+
+    //login
+    'login_welcome' =>  'Welcome Login Page',
+    'login_failed'  =>  '%1%: Username or Password not correct!',
+
+    //register translate
+    'register_welcome'  =>  'Welcome Register Page',
+
+    //users translate
+    'users_noactive'  =>  '%1%: Your account no active! Please check Inbox or Spam Mail for active this account!',
+
+    //validate
+    'username_double'  =>  '%1%: Username already exists in the system.',
+    'username_invalid'  =>  'Username must be letters, numbers not special characters.',
+    'username_length'   =>  'Username length required is %1% to %2% characters.',
+    'email_double'  =>  '%1%: Username already exists in the system.',
+    'email_invalid'  =>  'Email must be format email@domainemail.etc ',
+    'email_empty'    =>  'Email must be enter, can not empty.',
+    'password_length'   =>  'Password length required minimum is %1% characters.',
+    'password_verify_invalid'   =>  'Password Verify %1% not math with password.',
+    'fullname_length'   =>  'Fullname length required is %1% to %2% characters.',
+];
