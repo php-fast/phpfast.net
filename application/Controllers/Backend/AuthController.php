83f1c5edb6b1a2286340394b73df05f70ee3e8e6
truongthinh3902 <truongthinh3902@gmail.com>
2024-10-16 15:34:38 +0700

    edit postty

--- application/Controllers/Backend/AuthController.php
+++ application/Controllers/Backend/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends BaseController
     public function login()
     {
         //Buoc validate neu co request login.
-        if (    ('username')){
+        if (HAS_POST('username')){
             $csrf_token = S_POST('csrf_token') ?? '';
             if (!Session::csrf_verify($csrf_token)){
                 Session::flash('error', Flang::_e('csrf_failed') );
